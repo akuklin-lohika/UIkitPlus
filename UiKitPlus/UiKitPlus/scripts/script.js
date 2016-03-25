35f6c9c4f8a35d4581c91a1e6047ac215c8ee24c
Alexander Litvinov <alexandrl@123software.ru>
2016-03-25 17:37:18 +0200

    bug with FF

---
+++ UiKitPlus/UiKitPlus/scripts/script.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("input[name='check']").click(function (e) {
+        e.stopPropagation();
+    });
+});
+
+function test() {
+    var div = $('#clr').context.childNodes[1].childNodes[2].children[9].children[0];
+    div.zIndex = 2;
+}
+
+function prevent(e){
+    return false;
+}
